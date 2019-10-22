28f294f6b3a8970ecac07f6f68a933f699982de0
dcgarde <garde.danielle@gmail.com>
2019-10-22 15:48:54 -0500

    edited physicians file to show office locations

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -2,5 +2,8 @@ class Physician < ApplicationRecord
   has_many :offices, inverse_of: :physician
   accepts_nested_attributes_for :offices, reject_if: :all_blank, allow_destroy: true
 
+  has_many :appointments
+  has_many :patients
+
   validates_presence_of :physician_name, :specialty, on: :create, message: "can't be blank"
 end
